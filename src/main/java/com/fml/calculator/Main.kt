b39912d809aa8b37f8f06569fad2e30f91ea6f97
Scott Busche <swbusche@gmail.com>
2018-10-25 21:10:19 -0500

    Swap to new Kotlin style guide

--- src/main/java/com/fml/calculator/Main.kt
+++ src/main/java/com/fml/calculator/Main.kt
@@ -6,20 +6,20 @@ object Main {
     fun main(args: Array<String>) {
 
         val movies = this::class.java.classLoader.getResource("movies.csv")
-                .readText()
-                .split("\n")
-                .map { it.split(",") }
-                .map { Movie(it[0], Integer.parseInt(it[1].trim()), Integer.parseInt(it[2].trim())) }
+            .readText()
+            .split("\n")
+            .map { it.split(",") }
+            .map { Movie(it[0], Integer.parseInt(it[1].trim()), Integer.parseInt(it[2].trim())) }
 
         CombosWithReps(8, movies).getComboList()
-                .map { MovieTheater(it) }
-                .filter { it.totalCost() <= 1000 }
-                .groupBy { it.totalRevenue() }
-                .toSortedMap()
-                .toList()
-                .asReversed()
-                .take(5)
-                .forEach { println(it) }
+            .map { MovieTheater(it) }
+            .filter { it.totalCost() <= 1000 }
+            .groupBy { it.totalRevenue() }
+            .toSortedMap()
+            .toList()
+            .asReversed()
+            .take(5)
+            .forEach { println(it) }
 
     }
 }
